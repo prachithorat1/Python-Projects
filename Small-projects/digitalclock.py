444a6840070c5fa03a8535a7314ef762f06eae78
Prachi Thorat <108792209+prachithorat1@users.noreply.github.com>
2026-03-27 23:50:06 +0530

    Update digitalclock.py

--- Small-projects/digitalclock.py
+++ Small-projects/digitalclock.py
@@ -1,1 +1,19 @@
-import
+import tkinter as tk
+from time import strftime
+
+root = tk.Tk()
+root.title("Digital clock")
+
+def time():
+  string = strftime('%H:%M:%S %p \n %D')
+  label.config(text=string)
+  label.after(1000,time)  //update the time continuously
+
+//create object
+label = tl.Label(root, font=('calibri', 50, 'bold'), background='purple', foreground='black')
+label.pack(anchor='center')
+
+time()
+
+//keep in loop
+root.mainloop()
