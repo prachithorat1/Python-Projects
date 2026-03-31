fd6895cd29e6eae0bfecc89f3a9dcf261bd0b135
Prachi Thorat <108792209+prachithorat1@users.noreply.github.com>
2026-04-01 00:16:53 +0530

    Update scanner.py

--- Small-projects/scanner.py
+++ Small-projects/scanner.py
@@ -8,3 +8,35 @@ upi_id = input("Enter your UPI id = ")
 
 #upi://pay?ps=UPI_ID&apn=NAME&am=Amount$cu=CURRENCY&tn=MESSAGE
 
+
+import qrcode
+
+#Taking UPI ID as an input
+upi_id = input("Enter your UPI id = ")
+
+#upi://pay?ps=UPI_ID&apn=NAME&am=Amount$cu=CURRENCY&tn=MESSAGE
+
+# pa = upi id in ehich we have to do payment
+#
+# pn = recipent name
+#
+# am = amount
+#
+# cu = currency
+#
+# tn = payment message
+
+phone_url = f'upi://pay?pa={upi_id}&pn=Recipient%20Name&mc=1234'
+paytm_url = f'upi://pay?pa={upi_id}&pn=Recipient%20Name&mc=1234'
+google_pay_url = f'upi://pay?pa={upi_id}&pn=Recipient%20Name&mc=1234'
+
+#create QR code for each payment app
+
+phonepe_qr = qrcode.make(phonepe_url)
+paytm_qr = qrcode.make(paytm_url)
+google_pay_qr = qrcode.make(google_pay_url)
+
+#save the QR code to image file(optional)
+phonepe_qr.save('phonepe_qr.png')
+paytm_qr.save('paytm_qr.png')
+google_pay_qr.save('google_pay_qr.png')
